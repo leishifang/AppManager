1265bc4401f337913ce2bcce8006e9e53cc5587c
leishifang <353646121@qq.com>
2017-03-14 23:10:14 +0800

    memory and storage

---
+++ app/src/main/java/com/example/giggle/appmanager/MainActivity.java
@@ -0,0 +1,185 @@
+package com.example.giggle.appmanager;
+
+import android.animation.ValueAnimator;
+import android.app.ActivityManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.dinuscxj.progressbar.CircleProgressBar;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.progress_storage)
+    CircleProgressBar mProgressStorage;
+    @BindView(R.id.progress_memory)
+    CircleProgressBar mProgressMemory;
+    /**
+     * 已使用的内存百分比
+     */
+    private int radioUsedLast = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        initView();
+        updateStorageState();
+        updateMemoryState();
+        /**
+         * 每秒一次，更新内存使用情况
+         */
+        Observable.interval(1, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        updateMemoryState();
+                    }
+                });
+    }
+
+    public void updateMemoryState() {
+        ActivityManager.MemoryInfo info = new ActivityManager.MemoryInfo();
+        ((ActivityManager) getSystemService(ACTIVITY_SERVICE)).getMemoryInfo(info);
+        int ratioUsed = (int) ((info.totalMem - info.availMem) * 100 / info.totalMem);
+        ValueAnimator animator = ValueAnimator.ofInt(radioUsedLast, ratioUsed);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                mProgressMemory.setProgress((Integer) valueAnimator.getAnimatedValue());
+            }
+        });
+        animator.setDuration(1000);
+        animator.start();
+        radioUsedLast = ratioUsed;
+    }
+
+    public void updateStorageState() {
+        long totalSpace = 0;
+        long usableSpace = 0;
+        if (getExternalStorageState()) {
+            totalSpace += Environment.getExternalStorageDirectory().getTotalSpace();
+            usableSpace += Environment.getExternalStorageDirectory().getUsableSpace();
+        }
+        totalSpace += Environment.getDataDirectory().getTotalSpace();
+        usableSpace += Environment.getDataDirectory().getUsableSpace();
+        int ratioUsed = (int) ((totalSpace - usableSpace) * 100 / totalSpace);
+
+        ValueAnimator animator = ValueAnimator.ofInt(0, ratioUsed);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                mProgressStorage.setProgress((int) valueAnimator.getAnimatedValue());
+            }
+        });
+        animator.setDuration(2000);
+        animator.start();
+    }
+
+    private boolean getExternalStorageState() {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)
+                && Environment.isExternalStorageRemovable();
+    }
+
+    private void initView() {
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
