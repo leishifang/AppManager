6406d5904f5e454da9956c31edf69bd104c0bf85
leishifang <353646121@qq.com>
2017-03-16 11:26:51 +0800

    applist

---
+++ app/src/main/java/com/example/giggle/appmanager/ui/AppListActivity.java
@@ -0,0 +1,11 @@
+package com.example.giggle.appmanager.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by leishifang on 2017/3/16 10:55.
+ */
+
+public class AppListActivity extends AppCompatActivity {
+
+}
