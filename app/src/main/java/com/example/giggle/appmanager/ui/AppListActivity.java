70e0208bf9252264fa12f9ce1ace2fdee8e5c53f
leishifang <353646121@qq.com>
2017-03-16 18:25:52 +0800

    applist

--- app/src/main/java/com/example/giggle/appmanager/ui/AppListActivity.java
+++ app/src/main/java/com/example/giggle/appmanager/ui/AppListActivity.java
@@ -1,6 +1,13 @@
 package com.example.giggle.appmanager.ui;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.example.giggle.appmanager.R;
 
 /**
  * Created by leishifang on 2017/3/16 10:55.
@@ -8,4 +15,13 @@ import android.support.v7.app.AppCompatActivity;
 
 public class AppListActivity extends AppCompatActivity {
 
+    private static final String TAG = "APPLISTACTIVITY";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_layout_app);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
 }
